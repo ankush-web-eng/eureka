4648fe49300caa07c0b991522e429555004c9645
Ankush <deshwalankush23@gmail.com>
2024-07-16 10:00:04 +0530

    Changed the version of Next.js and added authentication in Patient side

---
+++ apps/patient/app/page.tsx
@@ -0,0 +1,5 @@
+import Auth from "@/components/ui/Auth";
+
+export default function Home() {
+  return <Auth /> 
+}
---
+++ apps/patient/middleware.ts
@@ -0,0 +1,21 @@
+
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export default async function middleware(request: NextRequest) {
+
+    const token = await getToken({ req: request })
+    const url = new URL(request.url)
+    
+    if (token && url.pathname === '/') {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    } else if(!token && url.pathname === '/dashboard') {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/', '/dashboard']
+};
